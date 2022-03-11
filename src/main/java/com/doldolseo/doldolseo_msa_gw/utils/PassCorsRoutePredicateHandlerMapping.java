2bc30f16b50aa5835d8d246746c174f1b3d3bbe0
kki7823 <kjd55399997@gmail.com>
2022-03-11 17:05:16 +0900

    CORS 설정

--- src/main/java/com/doldolseo/doldolseo_msa_gw/utils/PassCorsRoutePredicateHandlerMapping.java
+++ src/main/java/com/doldolseo/doldolseo_msa_gw/utils/PassCorsRoutePredicateHandlerMapping.java
@@ -9,10 +9,11 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 /*
-    Default로 설정되어 있는 CORS Prefilght 요청에대한 응답 로직 비활성
-    해당 로직이 활성 되어 있으면, 브라우저의 CORS Prefilgnr요청을 서비스가 아닌 Gw가 응답처리를 하게 됨
+    Spring cloud gateway에 Default로 설정되어 있는 CORS Prefilght 요청에 대한 응답 로직 비활성
+    해당 로직이 활성 되어 있으면, 브라우저의 CORS Prefilght 요청을  각 마이크로 서비스가 아닌 게이트웨이가 응답처리를 하게 됨
     코드 출처 : https://yangbongsoo.tistory.com/18
 */
+
 public class PassCorsRoutePredicateHandlerMapping extends RoutePredicateHandlerMapping {
 
     public PassCorsRoutePredicateHandlerMapping(FilteringWebHandler webHandler,
